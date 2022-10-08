c995c789b51f770e6c740aa1748a708de831c89d
sally <105549970+SSaallyy@users.noreply.github.com>
2022-10-08 14:04:39 +0900

    ***fix[estimateInquiry.html] -- 타임리프 세팅 -- location 셀렉트 박스 타임리프설정 완료
    *EstimateController.java
    -- 게시판 페이지 처리 코드 추가
    -- 업로드 후 게시판 이동 매핑
    
    *estimateList.html
    -- 게시글 타임리프 반복문처리(하단에 페이징버튼 추가해야함)
    
    *EstimateReplyRepository.java 생성
    EstimateReplyService.java 생성
    
    *Package.java / PackageDate.java 테이블 관계설정

--- src/main/java/com/project/tour/controller/EstimateController.java
+++ src/main/java/com/project/tour/controller/EstimateController.java
@@ -1,8 +1,17 @@
 package com.project.tour.controller;
 
+import com.project.tour.domain.EstimateInquiry;
+import com.project.tour.domain.EstimateInquiryForm;
+import com.project.tour.service.EstimateInquiryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,18 +19,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/estimate")
 public class EstimateController {
 
+    private final EstimateInquiryService estimateInquiryService;
+
     @GetMapping("/list")
-    public String estimateList() {
+    public String estimateList(Model model, @PageableDefault Pageable pageable) {
+
+        Page<EstimateInquiry> paging = estimateInquiryService.getList(pageable);
+
+        model.addAttribute("paging",paging);
+
         return "estimate/estimateList";
     }
 
     @GetMapping("/upload")
-    public String estimateInquiry() {
+    public String estimateInquiryget(Model model) {
+        model.addAttribute("estimateInquiryForm", new EstimateInquiryForm());
         return "estimate/estimateInquiry";
     }
 
-    @GetMapping("/inquiry/article")
-    public String estimateInquiryArticle() {
+
+    @PostMapping("/upload")
+    public String estimateInquirypost(EstimateInquiryForm estimateInquiryForm) {
+
+        estimateInquiryService.create(estimateInquiryForm);
+
+        return "redirect:/estimate/list";
+    }
+
+    @GetMapping("/inquiry/article/{id}")
+    public String estimateInquiryArticleget(@PathVariable("id") Integer id,EstimateInquiryForm estimateInquiryForm) {
+
+
+
+
         return "estimate/estimateInquiryArticle";
     }
 
--- src/main/java/com/project/tour/domain/Package.java
+++ src/main/java/com/project/tour/domain/Package.java
@@ -2,12 +2,10 @@ package com.project.tour.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,23 +13,42 @@ import java.time.LocalDateTime;
 public class Package {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long packageNum;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "packageNum")
+    private Integer id;
 
     private String location;
+
+    @Column(length = 300)
     private String packageName;
+
     private String hotelName;
+
     private LocalDateTime postStart;
     private LocalDateTime postEnd;
+
     private Integer count;
+
     private Integer travelPeriod;
+
     private String previewImage;
     private String detailImage;
+
+    @Column(length = 3000)
     private String packgeInfo;
+
     private Integer hitCount;
+
     private String keyword;
+    
+    /**항공사 이름*/
     private String transport;
 
+    /**하나의 패키지상품에 여러개의 출발일 
+     * 패키지 삭제시 각 출발일 상세 데이터 함께 삭제
+     * */
+    @OneToMany(mappedBy = "packages",cascade = CascadeType.REMOVE)
+    private List<PackageDate> PackageDateList;
 
 
 }
