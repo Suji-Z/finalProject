5947cdacb52abc436ea9b8d0b57a76356a0254f5
jungm2n2 <sjskrh05@gmail.com>
2022-10-12 19:00:27 +0900

    Review 파일 업로드 셋팅 & 테이블생성

---
+++ src/main/java/com/project/tour/domain/Review.java
@@ -0,0 +1,52 @@
+package com.project.tour.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reviewNum")
+    private Long id;
+
+    @Column (length = 200)
+    private String Subject;
+
+    @Column(length = 3000)
+    private String content;
+
+    @Column (length = 100)
+    private String reviewImage;
+
+    private LocalDateTime created;
+
+    @Column
+    private Double score;
+
+    //하나의 리뷰에 많은 댓글
+    @OneToMany(mappedBy = "reviewNum", cascade = CascadeType.REMOVE)
+    private List<Review_reply> replyList;
+
+    @ManyToMany
+    Set<Member> voter;
+
+    @Transient
+    public String getPhotosImagePath() {
+        if (reviewImage == null || id == null) return null;
+
+        return "/review-photo/" + id + "/" + reviewImage;
+    }
+
+
+
+}
