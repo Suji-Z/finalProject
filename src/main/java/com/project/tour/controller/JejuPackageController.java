e398f422e102c47fe9f2bfc75201fcebd3ebf6bb
sally <105549970+SSaallyy@users.noreply.github.com>
2022-10-15 00:03:21 +0900

    * form-dropdown.js -- 여행객 합산 값 미반영 오류 해결 -- 성인/어린이/유아 값 분리 적용 및 최소값 0 설정
    * nouislider.min.js
    -- 가격필터 적용 예정으로 코드 정렬
    
    * JejuPackageController
    -- 전체리스트 출력 페이징처리 구현
    
    * packagelist.html
    -- 페이징처리 및 데이터조회 확인 세부내용 적용예정

--- src/main/java/com/project/tour/controller/JejuPackageController.java
+++ src/main/java/com/project/tour/controller/JejuPackageController.java
@@ -1,7 +1,14 @@
 package com.project.tour.controller;
 
+import com.project.tour.domain.Package;
+import com.project.tour.service.PackageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +18,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/jeju")
 public class JejuPackageController {
 
+    @Autowired
+    private final PackageService packageService;
+
     @GetMapping("/list")
-    public String packagelist() {
+    public String packagelist(Model model, @PageableDefault Pageable pageable) {
+
+        Page<Package> paging = packageService.getList(pageable);
+
+        model.addAttribute("paging",paging);
+
         return "jejuPackage/packagelist";
     }
 
@@ -21,11 +36,12 @@ public class JejuPackageController {
 
 
 
+
         return "jejuPackage/packagelist";
     }
 
-    @GetMapping("/{location}/detail")
-    public String packagedetail(@PathVariable("location") String location2){
+    @GetMapping("/{location}/{id}")
+    public String packagedetail(@PathVariable("location") String location,@PathVariable("id") Long id){
 
 
 
