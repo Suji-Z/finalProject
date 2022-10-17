abc3f0d44c0e7597dbd61dc70b5a6d272d9edf87
januarysh <sunoeyh@daum.net>
2022-10-17 17:50:41 +0900

    관리자 페이지 html 및 패키지 상품 업로드 (패키지 요금 수정 예정)

---
+++ src/main/java/com/project/tour/domain/QnAReplyForm.java
@@ -0,0 +1,9 @@
+package com.project.tour.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QnAReplyForm {
+}
---
+++ src/main/java/com/project/tour/service/QnAReplyService.java
@@ -0,0 +1,9 @@
+package com.project.tour.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QnAReplyService {
+}
