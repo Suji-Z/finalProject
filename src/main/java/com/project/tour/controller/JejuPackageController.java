6cbeebd021d56228d5cd0234de6413c557bff674
sally <105549970+SSaallyy@users.noreply.github.com>
2022-10-18 00:00:44 +0900

    * packagelist.html --패키지 가격과 할인율 출력
    * JejuPackageController.java
    * PackageRepository.java
    * PackageService.java
    -- 지역/출발예정일/여행객별 검색

--- src/main/java/com/project/tour/controller/JejuPackageController.java
+++ src/main/java/com/project/tour/controller/JejuPackageController.java
@@ -26,24 +26,28 @@ public class JejuPackageController {
     private final PackageDateService packagedateService;
 
 
-    /** 전체리스트 */
+    /**
+     * 전체리스트
+     */
     @GetMapping("/list")
     public String packagelist(Model model, @PageableDefault Pageable pageable) {
 
         Page<Package> paging = packageService.getList(pageable);
 
-        model.addAttribute("paging",paging);
+        model.addAttribute("paging", paging);
 
         return "jejuPackage/packagelist";
     }
 
-    /** 지역별 리스트 */
+    /**
+     * 지역별 리스트
+     */
     @GetMapping("/{location}")
-    public String packageLocation(Model model,@PathVariable("location") String location, @PageableDefault Pageable pageable) {
+    public String packageLocation(Model model, @PathVariable("location") String location, @PageableDefault Pageable pageable) {
 
-        Page<Package> paging = packageService.getLocationList(location,pageable);
+        Page<Package> paging = packageService.getLocationList(location, pageable);
 
-        model.addAttribute("paging",paging);
+        model.addAttribute("paging", paging);
 
         return "jejuPackage/packagelist";
     }
@@ -51,38 +55,64 @@ public class JejuPackageController {
 
     /**
      * 지역, 출발예정일, 여행객 상단바 검색버튼
-     * */
+     */
 
     @PostMapping("/search")
-    public String searchPackage(@RequestParam(value = "location",required = false) String location,
-                                @RequestParam(value = "date",required = false) String date,
-                                @RequestParam(value = "counthidden",required = false) Integer count,
-                                @PageableDefault Pageable pageable,Model model){
+    public String searchPackage(@RequestParam(value = "location", required = false) String location,
+                                @RequestParam(value = "date", required = false) String date,
+                                @RequestParam(value = "counthidden", required = false) Integer count,
+                                @PageableDefault Pageable pageable, Model model) {
 
         log.info(location);
         log.info(date);
         log.info(String.valueOf(count));
 
-        Page<Package> paging = packageService.getsearchList(location, date, count, pageable);
+        /** 인원값이 null일때 0으로 고정 */
+        if (count == null || count.equals("0")) {
+            count = 0;
+        }
 
-        model.addAttribute("paging",paging);
+        if (location == null || location.equals("")) {
+            location = "제주";
+            Page<Package> paging = packageService.getdatecountsearchList(location, date, count, pageable);
+            model.addAttribute("paging", paging);
+            return "jejuPackage/packagelist";
+        }
+
+        Page<Package> paging = packageService.getfullsearchList(location, date, count, pageable);
+
+        model.addAttribute("paging", paging);
 
         return "jejuPackage/packagelist";
     }
 
 
-    /** 상세페이지 */
+    /**
+     * 상세페이지
+     */
     @GetMapping("/{location}/{id}")
-    public String packagedetail(@PathVariable("location") String location,@PathVariable("id") Long id,Model model){
+    public String packagedetail(@PathVariable("location") String location, @PathVariable("id") Long id, Model model) {
 
         Package apackage = packageService.getPackage(id);
 
         //PackageDate packageDate = packagedateService.getPackageDate2(id);
 
-        model.addAttribute("package",apackage);
+        model.addAttribute("package", apackage);
         //model.addAttribute("packageDate",packageDate);
 
         return "jejuPackage/packagedetail";
     }
 
+    /**
+     * 상세페이지 여행날짜별 가격출력
+
+    @GetMapping("/dateprice")
+    public PackageDate packagedetail(Model model) {
+
+
+
+
+        return packagedateService.get
+    }
+     */
 }
