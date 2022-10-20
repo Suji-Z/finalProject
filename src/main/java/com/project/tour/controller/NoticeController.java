04228187af56d3b3071392f94a97be2969be7120
jenniepp <sj323130@nate.com>
2022-10-20 20:55:57 +0900

    공지사항 domain, contoller, html upload

--- src/main/java/com/project/tour/controller/NoticeController.java
+++ src/main/java/com/project/tour/controller/NoticeController.java
@@ -1,6 +1,8 @@
 package com.project.tour.controller;
 
 import com.project.tour.domain.UserBookingForm;
+import com.project.tour.oauth.dto.SessionUser;
+import com.project.tour.oauth.service.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +16,22 @@ import java.security.Principal;
 @RequestMapping("/notice")
 public class NoticeController {
 
-    @GetMapping()
-    public String bookingDetail() {
+    @GetMapping("/list")
+    public String bookingList() {
         return "notice/notice_list";
+    }
+
+    @GetMapping("/article")
+    public String bookingArticle(@LoginUser SessionUser user, Principal principal) {
+
+
+
+        return "notice/notice_article";
+    }
+
+    @GetMapping("/create")
+    public String bookingCreate() {
+        return "notice/notice_create";
     }
 
 }
