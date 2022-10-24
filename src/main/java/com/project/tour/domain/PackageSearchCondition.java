85a8d03984b94d5b15373df495687dea9270248d
sally <105549970+SSaallyy@users.noreply.github.com>
2022-10-24 13:58:14 +0900

    * 제주도패키지 검색기능 specification -> querydsl 변경

--- src/main/java/com/project/tour/domain/PackageSearchCondition.java
+++ src/main/java/com/project/tour/domain/PackageSearchCondition.java
@@ -1,18 +1,23 @@
 package com.project.tour.domain;
 
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class PackageSearchCondition {
 
+    private String location1;
     private String location2;
     private String startday;
-    private Integer totcount;
-    private String keyword;
-    private String transport;
-    private Integer travelPeriod;
     private Integer pricerangestr;
     private Integer pricerangeend;
+    private List<Integer> travelPeriod;
+    private List<String> transport;
+    private Integer totcount;
+    private String keyword;
+
+
+
 
 }
