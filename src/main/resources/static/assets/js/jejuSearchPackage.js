a8ff5cbf9c212a00cd15ad6038b8a2ff317af71f
sally <105549970+SSaallyy@users.noreply.github.com>
2022-10-24 10:58:56 +0900

    * 추천 패키지 체크박스 갯수 검증스크립트 추가
    * 상세페이지 날짜별 잔여수량에 따라 예약하기 버튼 비활성화 기능 추가

--- src/main/resources/static/assets/js/jejuSearchPackage.js
+++ src/main/resources/static/assets/js/jejuSearchPackage.js
@@ -3,24 +3,29 @@ function selectdatePackage(){
 
     const date = $('#datePicker').val();
     const packagenum = $('#packagenum').val();
-    const acount = $('#acount').html();
-    const ccount = $('#ccount').html();
-    const bcount = $('#bcount').html();
+    const acount = fnReplace($('#acount').html());
+    const ccount = fnReplace($('#ccount').html());
+    const bcount = fnReplace($('#bcount').html());
+    const remaincount = fnReplace($('#remaincount_hidden').val());
+    const totcount = acount+bcount+ccount;
 
     if(date!=null){
         $('.select_person_side').show();
+        $('.date_remaincount').show();
+    }
+
+    if(acount!=0 && remaincount>=totcount){
+     $('.tour_select_offer_bar_bottom_sally').show();
+     $('.tour_select_offer_bar_bottom_sally_x').hide();
+    }else if(acount==0 || remaincount<totcount){
+     $('.tour_select_offer_bar_bottom_sally').hide();
+     $('.tour_select_offer_bar_bottom_sally_x').show();
     }
 
     if(acount!=0){
      $('#adult_count_price').show();
-     $('.tour_select_offer_bar_bottom_sally').show();
-     $('.tour_select_offer_bar_bottom_sally_x').hide();
-
-
     }else{
      $('#adult_count_price').hide();
-     $('.tour_select_offer_bar_bottom_sally').hide();
-     $('.tour_select_offer_bar_bottom_sally_x').show();
     }
 
     if(bcount!=0){
@@ -47,7 +52,7 @@ function selectdatePackage(){
         contentType: "application/json",
           success: function (data) {
 
-            var totalcount;
+            var totalprice;
 
                 if (data) {
 
@@ -59,13 +64,17 @@ function selectdatePackage(){
                 $('#bookingbcount').val(data.bcount);
                 $('#bookingccount').val(data.ccount);
 
+                $('#package_remaincount').html(data.remaincount+'개');
+                $('#remaincount_hidden').val(data.remaincount);
+
+                //선택인원별 가격 정보 출력
                 if(data.discount==null){
 
                 $('#aprice').html(data.aprice.toLocaleString()+'원');
                 $('#cprice').html(data.cprice.toLocaleString()+'원');
                 $('#bprice').html(data.bprice.toLocaleString()+'원');
-                totalcount = data.aprice+data.cprice+data.bprice;
-                $('#totalprice').html(totalcount.toLocaleString()+'원');
+                totalprice = data.aprice+data.cprice+data.bprice;
+                $('#totalprice').html(totalprice.toLocaleString()+'원');
                 }else {
 
                 $('#bfaprice').html(data.aprice.toLocaleString()+'원');
@@ -74,8 +83,8 @@ function selectdatePackage(){
                 $('#aftaprice').html(data.dcaprice.toLocaleString()+'원');
                 $('#aftbprice').html(data.dcbprice.toLocaleString()+'원');
                 $('#aftcprice').html(data.dccprice.toLocaleString()+'원');
-                 totalcount = data.dcaprice+data.dcbprice+data.dccprice;
-                $('#totalprice').html(totalcount.toLocaleString()+'원');
+                 totalprice = data.dcaprice+data.dcbprice+data.dccprice;
+                $('#totalprice').html(totalprice.toLocaleString()+'원');
                 }
 
                } else {
