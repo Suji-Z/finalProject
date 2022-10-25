0ba85d26c6ccb8e18391458e178503db4561c98e
januarysh <sunoeyh@daum.net>
2022-10-25 20:37:41 +0900

    package 입력 폼 수정, header footer 링크 추가

--- src/main/java/com/project/tour/domain/PackageCreate.java
+++ src/main/java/com/project/tour/domain/PackageCreate.java
@@ -8,6 +8,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -16,39 +17,52 @@ import java.util.Date;
 @Setter
 public class PackageCreate {
 
-    @Size(max = 20)
+    @NotEmpty(message = "여행지를 선택해주세요.")
     private String location1;
 
+    @NotEmpty(message = "여행지를 선택해주세요")
     private String location2;
 
+    @NotEmpty(message = "패키지 이름을 입력하세요.")
     private String packageName;
 
+    @NotNull
     private Integer aprice;
 
+    @NotNull
     private Integer bprice;
 
+    @NotNull
     private Integer cprice;
 
-    private String HotelName;
+    @NotEmpty(message = "숙소 이름을 입력하세요")
+    private String hotelName;
 
+    @NotEmpty(message = "상품 게시일을 입력해주세요.")
     private String postStart;
 
+    @NotEmpty(message = "상품 게시 마감일을 입력해주세요.")
     private String postEnd;
 
+    @NotNull
     private Integer count;
 
     private Integer discount;
 
+    @NotNull
     private Integer travelPeriod;
 
+    @NotEmpty(message = "미리보기 이미지를 첨부해주세요.")
     private String previewImage;
 
+    @NotEmpty(message = "상품 상세 이미지를 첨부해주세요.")
     private String detailImage;
 
     private String packageInfo;
 
     private String hitCount;
 
+    @NotEmpty(message = "여행 키워드를 입력해주세요.")
     private String keyword;
 
     private Long packageNum;
