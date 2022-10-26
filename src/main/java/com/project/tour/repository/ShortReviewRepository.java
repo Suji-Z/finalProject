13a565e797e11a26c3f1c0eb3b196aa68f95de54
Kim Hee Jin <106804351+heeheejjin@users.noreply.github.com>
2022-10-26 19:17:58 +0900

    ShortReviewRepository 수정

--- src/main/java/com/project/tour/repository/ShortReviewRepository.java
+++ src/main/java/com/project/tour/repository/ShortReviewRepository.java
@@ -14,7 +14,7 @@ public interface ShortReviewRepository extends JpaRepository<ShortReview,Long> {
 
 
     //패키지와 관련된 리뷰만 출력할 리스트
-    List<ShortReview> findByPackages_Id(Package packages);
+    List<ShortReview> findAllByPackages(Package packages);
 
     Optional<ShortReview> findByIdAndPackages_Id(Long id, Long packageNum);
 
