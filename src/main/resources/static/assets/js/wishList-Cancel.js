b7a9802eefca0bcb4b170327272612516e9c5a64
jungmin <sjskrh05@gmail.com>
2022-10-31 18:01:39 +0900

    리뷰추천, 위시리스트 완성본 3차

--- src/main/resources/static/assets/js/wishList-Cancel.js
+++ src/main/resources/static/assets/js/wishList-Cancel.js
@@ -14,9 +14,14 @@ function wishListCancel(event){
         },
 
         success : function(data) {
+            if(confirm('위시리스트를 삭제하시겠습니까?')){
+                alert("삭제되었습니다.");
+                $('#wishListTable').replaceWith(data);
+            }else{
+                return;
+            }
 
-            alert("위시리스트가 삭제되었습니다.");
-            $('#wishListTable').replaceWith(data);
+
 
         },
         error: function(xhr, status, error) {
