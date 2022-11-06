b1818d1fcddbfbef7a142a8d7e6f8fd1d2429118
sally <105549970+SSaallyy@users.noreply.github.com>
2022-11-06 15:45:37 +0900

    fix: "패키지 게시기간에 따라 상세페이지 선택기간 제한"

--- src/main/resources/static/assets/js/calendar.js
+++ src/main/resources/static/assets/js/calendar.js
@@ -1,7 +1,7 @@
 $(function() {
 		$('#datePicker').datepicker({
 		    format: "yyyy-mm-dd",	//데이터 포맷 형식(yyyy : 년 mm : 월 dd : 일 )
-		    startDate: '0d',	//달력에서 선택 할 수 있는 가장 빠른 날짜. 이전으로는 선택 불가능 ( d : 일 m : 달 y : 년 w : 주)
+		    startDate: new Date($('#packagePostStart').val()),	//달력에서 선택 할 수 있는 가장 빠른 날짜. 이전으로는 선택 불가능 ( d : 일 m : 달 y : 년 w : 주)
 		    endDate: new Date($('#packagePostEnd').val()),
 		    autoclose : true,	//사용자가 날짜를 클릭하면 자동 캘린더가 닫히는 옵션
 		    templates : {
