fd4f4bdd0992168cbe6e7a67674d4e6cf6097997
januarysh <sunoeyh@daum.net>
2022-11-06 12:08:57 +0900

    package 등록 시 게시 기간 제약 조건 추가

--- src/main/resources/static/assets/js/admin-package-image.js
+++ src/main/resources/static/assets/js/admin-package-image.js
@@ -1,8 +1,16 @@
 function packageSubmit(){
     let image1 = $('#image1').val();
     let image2 = $('#image2').val();
+    let postStart = $('#postStart').val();
+    let postEnd = $('#postEnd').val();
 
-    if(image1==""||image1==null) {
+
+
+    if(postStart>postEnd) {
+        $("#postMessage").text("상품 게시기간을 확인해주세요.");
+        return false;
+    }
+    else if(image1==""||image1==null) {
         $("#image1Message").text("상품 미리보기 이미지를 확인해주세요.");
         return false;
     }else if(image2==""||image2==null){
